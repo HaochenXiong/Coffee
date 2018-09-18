67e3bb1a8dd337a8421d86b7576bb9dfd2b0b84d
HaochenXiong <42971668+HaochenXiong@users.noreply.github.com>
2018-09-18 20:19:08 +0800

    Update coffeetawk.js

--- coffeetawk.js
+++ coffeetawk.js
@@ -1,1 +1,14 @@
 //CoffeeTawk — by 'Haochen Xiong' — applab, lab 1, 2018
+
+/*
+Challenge 1:
+
+1 We have a larger demand here in Shanghai
+2 our stores don’t operate as efficiently
+3 The lines are long
+4 It takes a minute or more to take an order
+5 It takes 5 minutes for customer to walk out the door with their beverage
+6 Payment is quick
+7 Most people use AliPay or WeChat Pay
+8 The order-taking process is slow
+9 
